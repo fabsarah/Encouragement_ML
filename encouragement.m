f68a8f700c4145b40af6e58388425a4dda07ef7e
Sarah Faber <31863074+fabsarah@users.noreply.github.com>
2019-03-11 22:11:14 -0400

    Add files via upload

---
+++ encouragement.m
@@ -0,0 +1,26 @@
+function encouragement
+% A function for when the world is being a total jerk. Encouragement is a
+% tiny, adorabe (I assume) bot that lives in Matlab and dispenses
+% encouragement right to the command window.
+a = randi([1 10],1,1);
+if a==1
+    fprintf('You can do it! \n');
+elseif a==2
+    fprintf('I believe in you! \n');
+elseif a==3
+    fprintf('Your code is good and you should feel good! \n');
+elseif a==4
+    fprintf('You''re a fantastic scientist! \n');
+elseif a==5
+    fprintf('Who has infinite thumbs and thinks you''re great? Me! \n');
+elseif a==6
+    fprintf('If I could make hot beverages, I would make you a hot beverage! \n');
+elseif a==7
+    fprintf('Here is a Nobel prize! \n');
+elseif a==8
+    fprintf('You got this! \n');
+elseif a==9
+    fprintf('You can do the thing! \n');
+elseif a==10
+    fprintf('Believe in yourself! \n');
+end
